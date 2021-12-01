083edc3f0416eb0ee1841781fbf1b9e0fc767161
Deborah M <47586059+DMAIDOH@users.noreply.github.com>
2021-12-01 16:32:49 -0500

    hamburger icon functionality

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ var theDate = new Date();
 
 var footer = document.getElementById("footer-text");
 footer.textContent = "@Copyright " + theDate.getFullYear();
+
+//hamburger functionality
+var menuIcon = document.querySelector('.menu-icon');
+var menuItems = document.querySelector('.menu-items');
+
+
+menuIcon.addEventListener('click', function(){
+    if(menuItems.style.display== "none"){
+        menuItems.style.display = "flex";
+    }
+    else if(menuItems.style.display= "none"){
+        menuItems.style.display = "flex";
+    }
+}
+);
+
