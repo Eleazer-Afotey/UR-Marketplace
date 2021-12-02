b687bf16af9d55fa0e635a480da02ce3dade0b55
Eleazer Afotey <eleazerallnice@gmail.com>
2021-12-01 20:24:48 -0500

    added contact and appliances pages

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ var theDate = new Date();
 
 var footer = document.getElementById("footer-text");
 footer.textContent = "@Copyright " + theDate.getFullYear();
+
+var item = document.getElementById("item1");
+
+function show() {
+  console.log("clicked");
+}
+item.addEventListener("click", show);
+
+var bookItem = document.getElementById("item1-bookmark");
+function bookmark() {
+  if (bookItem.textContent === "bookmark_border") {
+    bookItem.textContent = "bookmark";
+    alert("Item bookmarked");
+  } else {
+    bookItem.textContent = "bookmark_border";
+  }
+}
+bookItem.addEventListener("click", bookmark);
