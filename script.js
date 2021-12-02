fb04417aa767bbe4ca561c8ed97e65e07f471496
Deborah M <47586059+DMAIDOH@users.noreply.github.com>
2021-12-02 03:04:38 -0500

    hamburger icon works now

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ var theDate = new Date();
 var footer = document.getElementById("footer-text");
 footer.textContent = "@Copyright " + theDate.getFullYear();
 
+//hamburger icon functionality
+function showMenu(){
+    /*when the user clicks on the hamburger,
+    toggle between hiding and showing the menu items*/
+    document.getElementById("menu-items").classList.toggle("show");
+}
+
+
 var item = document.getElementById("item1");
 
 function show() {
@@ -21,17 +29,6 @@ function bookmark() {
   }
 }
 bookItem.addEventListener("click", bookmark);
-//hamburger functionality
-var menuIcon = document.querySelector(".menu-icon");
-var menuItems = document.querySelector(".menu-items");
-
-menuIcon.addEventListener("click", function () {
-  if (menuItems.style.display == "none") {
-    menuItems.style.display = "flex";
-  } else if ((menuItems.style.display = "none")) {
-    menuItems.style.display = "flex";
-  }
-});
 
 var material = document.getElementById("item1-get");
 
