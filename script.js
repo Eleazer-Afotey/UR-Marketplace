1035248cf66e623f531eea8c8a9940de120dbd95
JoelT21 <Tabapssijoel@gmail.com>
2021-12-02 10:11:38 -0500

    changes 12-1

--- script.js
+++ script.js
@@ -4,56 +4,45 @@ var theDate = new Date();
 var footer = document.getElementById("footer-text");
 footer.textContent = "@Copyright " + theDate.getFullYear();
 
-<<<<<<< HEAD
 //bookmark
-var item_name = "samsung tv";
-// const myObj = { itm_name: item_name, itm_type: 31, itm_picture: "New York" };
-// const myJSON = JSON.stringify(myObj);
-// localStorage.setItem("testJSON", myJSON);
 var text = [
 {
   itm_name: "tv",
   icon_name: "local_pizza"
 },
-{
-  itm_name: "Icecream",
-  icon_name: "icecream"
-},
 ]
 
+var material = document.getElementById("item1-get");
 
-// let text = '{"items":[' +
-// '{"itm_name":`${item_name}`,"lastName":"Doe" },' +
-// '{"firstName":"Anna","lastName":"Smith" },' +
-// '{"firstName":"Peter","lastName":"Jones" }]}';
+//Check Bookmark
+function checkBookmark() {
+  return bookItem.textContent === "bookmark";
+}
 
-// const obj = JSON.parse(text);
-// document.getElementById("demo").innerHTML =
-// obj.employees[1].firstName + " " + obj.employees[1].lastName;
-
-// Retrieving data:
-//let text = localStorage.getItem("testJSON");
-
-text[0].itm_name = item_name;
+function bookmarkItem(){
+  if(checkBookmark()){
+    text[0].itm_name = material.textContent;
+  }
+}
 
 let data = [text[0].itm_name];
-      console.log(typeof text);
-      console.log( data);
-      let list = document.getElementById("bookmark_menu");
+console.log(typeof text);
+console.log( data);
+let list = document.getElementById("bookmark_menu");
 
-      data.forEach((item) => {
-        let li = document.createElement("li");
-        console.log(typeof item);
-        li.innerText = item;
-        list.appendChild(li);
-      });
+data.forEach((it) => {
+  let li = document.createElement("li");
+  console.log(typeof it);
+  li.innerText = it;
+  list.appendChild(li);
+
+  });
 
 
 
 //document.getElementById("bookmark_menu").innerHTML = obj.itm_name;
-=======
 var item = document.getElementById("item1");
-
+      console.log(item);
 function show() {
   console.log("clicked");
 }
@@ -81,10 +70,3 @@ menuIcon.addEventListener("click", function () {
   }
 });
 
-var material = document.getElementById("item1-get");
-
-//Check Bookmark
-function checkBookmark() {
-  return bookItem.textContent === "bookmark";
-}
->>>>>>> 35edd08dfa1a293c0f52ad784147f93603f354f6
